e504e3a65c3b21790fefd421d2dc97bd5ac3b1fc
Alex <Alexander.Troy.Scott@gmail.com>
2024-03-02 20:27:28 -0600

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -2,6 +2,11 @@
   description = "NixOS Server Flake";
 
   inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    # Home manager
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs-unstable";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     };
 
