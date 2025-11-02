677c99039d3df1bbabe89ca174e53cc08a6419a5
AlexanderTroyScott <Alexander.Troy.Scott@gmail.com>
2025-11-02 11:02:11 -0600

    delete comments

--- coral/docker.nix
+++ coral/docker.nix
@@ -1,19 +1,36 @@
-  { config, pkgs, ... }:
+  { lib, config, pkgs, ... }:
 
 {
   environment.systemPackages = with pkgs; [
     docker
   ];
+
+ #   users.groups.docker = {
+ #   name = "docker";
+ # };
+ # users.extraGroups.docker.members = ["actuary"];
   virtualisation.docker.enable = true;
   #virtualisation.docker.rootless = {
-   # enable = true;
-   # setSocketVariable = true;
+  #  enable = true;
+  #  setSocketVariable = true;
   #};
   virtualisation.docker.liveRestore = false;
   #virtualisation.docker.storageDriver = "btrfs";
 
   # (Optional) Enable Docker registry.
   services.dockerRegistry.enable = true;
-  # services.dockerRegistry.proxy.enable = true;
-
+  #virtualisation.docker.extraOptions = "--ipv6 --fixed-cidr-v6 fd00::/80";
+  
+  virtualisation.docker.daemon.settings = {
+    #data-root = "/home/das/docker/";
+    #userland-proxy = false;
+    #experimental = true;
+    ipv6 = true;
+    fixed-cidr-v6 = "fd00::/80";
+    #metrics-addr = "0.0.0.0:9323";
+    #log-driver = "json-file";
+    #log-opts.max-size = "10m";
+    #log-opts.max-file = "10";
+  };
 }
+
